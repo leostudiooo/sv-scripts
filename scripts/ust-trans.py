ae5d1c93721836181dcad40b3c9520fbb2fcd75f
Leo Li <lilingfeng0408@outlook.com>
2021-11-28 22:16:26 +0800

    Update ust-trans.py

--- scripts/ust-trans.py
+++ scripts/ust-trans.py
@@ -1,13 +1,12 @@
+# coding=utf-8
+# Written 22:15, Nov 28 2021, by Leo Li
+
 import sys
 import re
 
-if sys.argv[2]:
-    outputFile = open(sys.argv[2], "w", encoding="utf-8")
-else:
-    outputFile = open(sys.argv[1]+"- Transferred.UST", "w", encoding="utf-8")
-
+outputFile = open(sys.argv[2], "w", encoding="utf-8")
 with open(sys.argv[1], "r", encoding="shift-jis") as inputFile:
-    for ln in inputFile:
-        outputFile.write(re.sub(r"^Lyric=\S{1} ", "Lyric=", ln))
-
+	for ln in inputFile:
+		outputFile.write(re.sub(r"^Lyric=\S{1} ", "Lyric=", ln))
 outputFile.close()
+
