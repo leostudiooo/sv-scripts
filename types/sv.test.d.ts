af58b2e21f8ef16251e7573ddb28a4e082cc25a9
Li Lingfeng <lilingfeng0408@outlook.com>
2023-01-27 21:33:44 +0800

    Finished test.d.ts

--- types/sv.test.d.ts
+++ types/sv.test.d.ts
@@ -1,5 +1,5 @@
 declare module SV {
-	const QUARTER: number;
+	const QUARTER: number = 705600000;
 	const blackKey: (k: number) => boolean;
 	const blick2Quarter: (b: number) => number;
 	const blick2Seconds: (b: number, bpm: number) => number;
@@ -32,6 +32,178 @@ declare module SV {
 	const showYesNoCancelBoxAsync: (title: string, message: string, callback: (ret: string) => any) => void;
 	const T: (text: string) => string;
 
+	class ArrangementSelectionState extends NestedObject, SelectionStateBase, GroupSelection {}
+
+	class ArrangementView extends NestedObject {
+		getNavigation: () => CoordinateSystem;
+		getSelection: () => ArrangementSelectionState;
+	}
+	
+	class Automation extends NestedObject {
+		add: (b: number, v: number) => boolean;
+		clone: () => Automation;
+		get: (b: number) => number;
+		getAllPoints: () => Array<Array<number>>;
+		getDefinition: () => object;
+		getInterpolationMethod: () => string;
+		getLinear: (b: number) => number;
+		getPoints: (begin: number, end: number) => Array<Array<number>>;
+		remove: (b: number) => boolean;
+		removeAll: () => void;
+		simplify: (begin: number, end: number, threshold?: number) => boolean;
+	}
+	
+	class CoordinateSystem extends NestedObject {
+		getTimePxPerUnit: () => number;
+		getTimeViewRange: () => Array<number>;
+		getValuePxPerUnit: () => number;
+		getValueViewRange: () => Array<number>;
+		setTimeLeft: (time: number) => void;
+		setTimeRight: (time: number) => void;
+		setTimeScale: (scale: number) => void;
+		setValueCenter: (v: number) => void;
+		snap: (b: number) => void;
+		t2x: (t: number) => void;
+		v2y: (v: number) => void;
+		x2t: (x: number) => void;
+		y2v: (y: number) => void;
+	}
+	
+	class GroupSelection {
+		clearGroups: () => boolean;
+		getSelectedGroups: () => Array<NoteGroupReference>;
+		hasSelectedGroups: () => boolean;
+		selectGroup: (reference: NoteGroupReference) => void;
+		unselectGroup: (reference: NoteGroupReference) => boolean;
+	}
+	
+	class MainEditorView extends NestedObject {
+		getCurrentGroup: () => NoteGroupReference;
+		getCurrentTrack: () => Track;
+		getNavigation: () => CoordinateSystem;
+		getSelection: () => TrackInnerSelectionState;
+	}
+	
+	class NestedObject {
+		getIndexInParent: () => number;
+		getParent: () => NestedObject | undefined;
+		isMemoryManaged: () => boolean;
+	}
+	
+	class Note extends NestedObject {
+		clone: () => Note;
+		getAttributes: () => object;
+		getDuration: () => number;
+		getEnd: () => number;
+		getLyrics: () => string;
+		getOnset: () => number;
+		getPhonemes: () => string;
+		getPitch: () => number;
+		setAttributes: (attributes: object) => void;
+		setDuration: (t: number) => void;
+		setLyrics: (lyrics: string) => void;
+		setOnset: (t: number) => void;
+		setPitch: (pitchNumber: number) => void;
+		setTimeRange: (onset: number, duration: number) => void;
+	}
+	
+	class NoteGroup extends NestedObject {
+		addNote: (note: Note) => number;
+		clone: () => NoteGroup;
+		getName: () => string;
+		getNote: (index: number) => Note;
+		getNumNotes: () => number;
+		getParameter: (type: string) => Automation;
+		getUUID: () => string;
+		removeNote: (index: number) => void;
+		setName: (name: string) => void;
+	}
+	
+	class NoteGroupReference extends NestedObject {
+		clone: () => NoteGroupReference;
+		getDuration: () => number;
+		getEnd: () => number;
+		getOnset: () => number;
+		getPitchOffset: () => number;
+		getTarget: () => NoteGroup;
+		getTimeOffset: () => number;
+		getVoice: () => object;
+		isInstrumental: () => boolean;
+		isMain: () => boolean;
+		setPitchOffset: (pitchOffset: number) => void;
+		setTarget: (group: NoteGroup) => void;
+		setVoice: (attributes: object) => void;
+	}
+	
+	class SelectionStateBase {
+		clearAll: () => boolean;
+		hasSelectedContent: () => boolean;
+		hasUnfinishedEdits: () => boolean;
+	}
+	
+	class PlayBackControl extends NestedObject {
+		getPlayhead: () => number;
+		getStatus: () => string;
+		loop: (beginSecond: number, endSecond: number) => void;
+		pause: () => void;
+		play: () => void;
+		seek: (time: number) => void;
+		stop: () => void;
+	}
+	
+	class Project extends NestedObject {
+		addNoteGroup: (group: NoteGroup, index?: number) => number;
+		addTrack: (track: Track) => number;
+		getDuration: () => number;
+		getFileName: () => string;
+		getNoteGroup: (id: number | string) => NoteGroup | undefined;
+		getNumNoteGroupsInLibrary: () => number;
+		getNumTracks: () => number;
+		getTimeAxis: () => TimeAxis;
+		getTrack: () => Track;
+		newUndoRecord: () => void;
+		removeNoteGroup: (index: number) => void;
+		removeTrack: (index: number) => void;
+	}
 	
 
+	class TimeAxis extends NestedObject {
+		addMeasureMark: (measure: number, numerator: number, denominator: number) => void;
+		addTempoMark: (b: number, bpm: number) => void;
+		clone: () => TimeAxis;
+		getAllMeasureMarks: () => Array<object>;
+		getAllTempoMarks: () => Array<object>;
+		getBlickFromSeconds: (t: number) => number;
+		getMeasureAt: (b: number) => number;
+		getMeasureMarkAt: (measureNumber: number) => object;
+		getMeasureMarkAtBlick: (b: number) => object;
+		getSecondsFromBlick: (b: number) => number;
+		getTempoMarkAt: (b: number) => number;
+		removeMeasureMark: (measure: number) => boolean;
+		removeTempoMark: (b: number) => boolean;
+	}
+	
+	class Track extends NestedObject {
+		addGroupReference: (group: NoteGroupReference) => number;
+		clone: () => Track;
+		getDisplayColor: () => string;
+		getDisplayOrder: () => number;
+		getDuration: () => number;
+		getGroupReference: (index: number) => NoteGroupReference;
+		getName: () => string;
+		getNumGroups: () => string;
+		isBounced: () => boolean;
+		removeGroupReference: (index: number) => void;
+		setBounced: (enabled: boolean) => void;
+		setDisplayColor: (hexColor: string) => void;
+		setName: (name: string) => void;
+	}
+	
+	class TrackInnerSelectionState extends NestedObject, SelectionStateBase, GroupSelection {
+		clearNotes: () => boolean;
+		getSelectedNotes: () => Array<Note>;
+		hasSelectedNotes: () => boolean;
+		selectNote: (note: Note) => void;
+		unselectNote: (note: Note) => boolean;
+	}
 }
